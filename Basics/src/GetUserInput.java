8f9394d6f889736f6495a4844e66c05cd7f6be4f
Jermaine <jermerf@gmail.com>
2020-04-13 15:56:37 -0300

    Changes from April 13

---
+++ Basics/src/GetUserInput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class GetUserInput {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("How old are you?");
+        int age = input.nextInt();
+
+        for(int i=0; i < age; i++){
+            System.out.println("Note for year " + i);
+        }
+    }
+
+}
---
+++ Basics/src/MyMain.java
@@ -0,0 +1,52 @@
+public class MyMain {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+
+        int yourAge = 10;
+        int myAge = 20;
+        int ourAges = yourAge / myAge;
+
+        // Your age is 10
+        yourAge = yourAge + 2;
+
+        yourAge ++;
+        yourAge --;
+        yourAge += 10;
+        yourAge -= 10;
+
+        boolean edisonIsOld = true;
+        int edisonAge = 4;
+
+        edisonIsOld = edisonAge > 10;
+
+        boolean bothTrue = 42 == 42;
+        boolean oneTOneF = 42 != 79;
+
+        boolean thisIsTrue = true;
+        thisIsTrue = !thisIsTrue;
+        thisIsTrue = !thisIsTrue;
+        thisIsTrue = !thisIsTrue;
+        thisIsTrue = !thisIsTrue;
+        thisIsTrue = !thisIsTrue;
+        thisIsTrue = !thisIsTrue;
+        thisIsTrue = !thisIsTrue;
+        thisIsTrue = !thisIsTrue;
+
+        boolean a = true;
+        boolean b = false;
+
+        boolean c = a && b; // True if both a and b are true
+        boolean d = a || b; // True if at least one of a or b are true
+
+        int bigNum = Integer.MAX_VALUE;
+
+        if(edisonAge > 3) {
+            System.out.println("Edison is full grown");
+        }else {
+            System.out.println("Edison is still growing");
+        }
+
+    }
+
+}
